9116debbbc341359ed06005bdedaf3b8e73e2910
Pacdor <pacdor@example.com>
2026-02-01 18:16:32 +0100

    Menú

---
+++ src/Controller/NearbyUsersController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NearbyUsersController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('nearby_users/index.html.twig', [
+            'controller_name' => 'NearbyUsersController',
+        ]);
+    }
+}
